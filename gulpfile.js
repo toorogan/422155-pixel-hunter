d808351472dde551ef817c1b1783b4e89fcf3653
Stepan Kondrat <stepan.kondrat@mail.ru>
2018-12-22 16:40:47 +0300

    added chai and mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 const del = require(`del`);
 const gulp = require(`gulp`);
 const sass = require(`gulp-sass`);
@@ -13,41 +12,50 @@ const imagemin = require(`gulp-imagemin`);
 const svgstore = require(`gulp-svgstore`);
 const rollup = require(`gulp-better-rollup`);
 const sourcemaps = require(`gulp-sourcemaps`);
+const mocha = require(`gulp-mocha`);
+const commonjs = require(`rollup-plugin-commonjs`);
 
 gulp.task(`style`, () => {
-  return gulp.src(`sass/style.scss`).
-    pipe(plumber()).
-    pipe(sass()).
-    pipe(postcss([
-      autoprefixer({
-        browsers: [
-          `last 1 version`,
-          `last 2 Chrome versions`,
-          `last 2 Firefox versions`,
-          `last 2 Opera versions`,
-          `last 2 Edge versions`
-        ]
-      }),
-      mqpacker({sort: true})
-    ])).
-    pipe(gulp.dest(`build/css`)).
-    pipe(server.stream()).
-    pipe(minify()).
-    pipe(rename(`style.min.css`)).
-    pipe(gulp.dest(`build/css`));
+  return gulp
+    .src(`sass/style.scss`)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(
+        postcss([
+          autoprefixer({
+            browsers: [
+              `last 1 version`,
+              `last 2 Chrome versions`,
+              `last 2 Firefox versions`,
+              `last 2 Opera versions`,
+              `last 2 Edge versions`
+            ]
+          }),
+          mqpacker({sort: true})
+        ])
+    )
+    .pipe(gulp.dest(`build/css`))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename(`style.min.css`))
+    .pipe(gulp.dest(`build/css`));
 });
 
 gulp.task(`sprite`, () => {
-  return gulp.src(`img/sprite/*.svg`)
-  .pipe(svgstore({
-    inlineSvg: true
-  }))
-  .pipe(rename(`sprite.svg`))
-  .pipe(gulp.dest(`build/img`));
+  return gulp
+    .src(`img/sprite/*.svg`)
+    .pipe(
+        svgstore({
+          inlineSvg: true
+        })
+    )
+    .pipe(rename(`sprite.svg`))
+    .pipe(gulp.dest(`build/img`));
 });
 
 gulp.task(`scripts`, () => {
-  return gulp.src(`js/**/*.js`)
+  return gulp
+    .src(`js/**/*.js`)
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(rollup({}, `iife`))
@@ -56,26 +64,28 @@ gulp.task(`scripts`, () => {
 });
 
 gulp.task(`imagemin`, [`copy`], () => {
-  return gulp.src(`build/img/**/*.{jpg,png,gif}`).
-    pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true})
-    ])).
-    pipe(gulp.dest(`build/img`));
+  return gulp
+    .src(`build/img/**/*.{jpg,png,gif}`)
+    .pipe(
+        imagemin([
+          imagemin.optipng({optimizationLevel: 3}),
+          imagemin.jpegtran({progressive: true})
+        ])
+    )
+    .pipe(gulp.dest(`build/img`));
 });
 
 gulp.task(`copy-html`, () => {
-  return gulp.src(`*.{html,ico}`).
-    pipe(gulp.dest(`build`)).
-    pipe(server.stream());
+  return gulp
+    .src(`*.{html,ico}`)
+    .pipe(gulp.dest(`build`))
+    .pipe(server.stream());
 });
 
 gulp.task(`copy`, [`copy-html`, `scripts`, `style`, `sprite`], () => {
-  return gulp.src([
-    `fonts/**/*.{woff,woff2}`,
-    `img/*.*`
-  ], {base: `.`}).
-    pipe(gulp.dest(`build`));
+  return gulp
+    .src([`fonts/**/*.{woff,woff2}`, `img/*.*`], {base: `.`})
+    .pipe(gulp.dest(`build`));
 });
 
 gulp.task(`clean`, () => {
@@ -113,5 +123,21 @@ gulp.task(`build`, [`assemble`], () => {
   gulp.start(`imagemin`);
 });
 
-gulp.task(`test`, () => {
+gulp.task(`test`, function () {
+  return gulp
+    .src([`js/**/*.test.js`])
+    .pipe(
+        rollup(
+            {
+              plugins: [commonjs()]
+            },
+            `cjs`
+        )
+    )
+    .pipe(gulp.dest(`build/test`))
+    .pipe(
+        mocha({
+          reporter: `spec`
+        })
+    );
 });
